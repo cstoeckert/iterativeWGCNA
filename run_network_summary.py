beb82377ed9bb37eb9036000fa4aaa149f8ec403
fossilfriend <egreenfest@gmail.com>
2016-09-21 15:52:58 -0400

    added logging
    and try catch block

--- run_network_summary.py
+++ run_network_summary.py
@@ -12,16 +12,30 @@ specify an module or specific iteration
 # Installation workaround - see README
 # import readline
 
+import logging
 from iterativeWGCNA.cmlargs import parse_summary_command_line_args
 from iterativeWGCNA.iterativeWGCNA import IterativeWGCNA
 from iterativeWGCNA.network import Network
 
 if __name__ == '__main__':
     args = parse_summary_command_line_args()
-    alg = IterativeWGCNA(args) # initialize workspace, logs and load profiles
-    network = Network(args)
-    network.build_from_file(alg.profiles)
-    network.summarize()
+    alg = IterativeWGCNA(args, True) # initialize workspace, logs and load profiles
+    logger = logging.getLogger('iterativeWGCNA.SummarizeResults')
+
+    try:
+        network = Network(args)
+        network.build_from_file(alg.profiles)
+        # network.summarize_network()
+    except Exception:
+        if logger is not None:
+            logger.exception('Summary: FAIL')
+        else:
+            raise
+    #finally:
+        #if logger is not None:
+            #logger.info(strftime("%c"))
+
+    
 
 __author__ = 'Emily Greenfest-Allen'
 __copyright__ = 'Copyright 2016, University of Pennsylvania'
